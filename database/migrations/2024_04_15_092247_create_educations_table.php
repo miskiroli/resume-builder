aa75ba02b2984fbfd2f9228f38a6a449374fd53b
Miski Roland <48199690+miskiroli@users.noreply.github.com>
2024-04-15 11:41:48 +0200

    Tables

---
+++ database/migrations/2024_04_15_092247_create_educations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('educations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('degree')->nullable();
+            $table->string('school');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->string('field_of_study');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('educations');
+    }
+};
